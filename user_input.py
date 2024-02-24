91b72aab646767925f5a11cdc0b121fd0a09044c
James Matata <138303825+JamesMatata@users.noreply.github.com>
2024-02-24 12:07:03 +0300

    Add files via upload

---
+++ user_input.py
@@ -0,0 +1,6 @@
+print("***PLEASE ENTER YOUR DETAILS BELOW****")
+name = input("Name: ")
+age = input("Age : ")
+location = input("Location: ")
+
+print("Hello " + name + ",you are now " + age + " years old. How is life in " + location)
